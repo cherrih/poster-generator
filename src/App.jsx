ac4324ed496284fe3772e8669e3891103af87814
cherrih <cherri.hartigan@gmail.com>
2019-05-29 12:31:03 -0700

    Initialize canvas and image placing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isImagePlacer: false,
+      currImage: null
+    };
+    this.canvasRef = React.createRef();
+    this.onImageClick = this.onImageClick.bind(this);
+    this.onCanvasClick = this.onCanvasClick.bind(this);
+  }
 
+  onImageClick(e) {
+    this.setState({
+      isImagePlacer: true,
+      currImage: e.target
+    })
+  }
+
+  onCanvasClick(e) {
+    const { currImage, isImagePlacer } = this.state;
+    if (isImagePlacer) {
+      // add pic to canvas at e.pageX, e.pageY
+      const canvas = this.canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      const width = canvas.width;
+      const height = canvas.height;
+      ctx.drawImage(currImage, e.pageX, e.pageY);
+      this.setState({
+        isImagePlacer: false,
+        currImage: null
+      })
+    }
+  }
 
   render() {
     return (
-      <div>Hello</div>
-    )
+      <>
+        <canvas width="500" height="500" ref={this.canvasRef} onClick={this.onCanvasClick}/>
+        <img src="../public/shy.jpg" onClick={this.onImageClick}/>
+      </>
+    );
   }
 }
 
